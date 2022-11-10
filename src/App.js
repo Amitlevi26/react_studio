32ba667e171e58c9c65d189be4735d18eaac3552
Amitlevi26 <77810143+Amitlevi26@users.noreply.github.com>
2022-11-09 21:31:57 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,92 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
 
-/* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
+// /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/" + item.image;
 });
-/* ############################################################## */
+// /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
-
+  const [cart, setCart] = useState([]);
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
+      {/*  */}
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        // in props, sets up a field called 'item' and passes in the 'item' variable
+        <BakeryItem item={item} cart={cart} setCart={setCart} count={count} setCount={setCount}/>
+        // <p>Bakery Item {index}</p> // replace with BakeryItem component
       ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-      </div>
+      {/* display what's in my cart */}
+      {cart.map((item) => (
+        <div>{item.name}</div>
+      ))
+      }
+      <div>{count}</div>
     </div>
-  );
+  )
+  // print total cost --> take everything in cart, counter for prices, similar to map --> calc cost and display it
 }
+
+// function CartItems(props) {
+//   return (
+//     <button onClick={() => props.updateCart(props.index)}>{propx.item.name}</button>
+//   )
+// }
+
+//TODO UPDATED get description, price and image to display
+
+// function App() {
+//   // TODO: use useState to create a state variable to hold the state of the cart
+//   /* add your cart state code here */
+//   // const [cart, setCart] = useState({});
+//   // const updateCart = (uid) => {}
+//   //   let newCart = cart;
+//   //   if (newCart[uid]) {
+//   //     newCart[uid] += 1;
+//   //   } else {
+//   //     newCart[uid] = 1;
+//   //   }
+//   //   setCart({...newCart});
+//   // }
+//   // return (
+//   //   <div>
+//   //     {data.map((item, index) => {
+//   //       return (
+//   //         <CartItems updateCart = {updateCart} item={item} index={index} />
+//   //       )
+//   //     })}
+//   //     {Object.keys(cart).map((key) => {
+//   //       return (
+//   //         data[key].name + ": " + cart[key] + " "
+//   //       )
+//   //     })}
+//   //     </div>
+//   //     )
+
+//   //   function CartItems(props) {
+//   //     return (
+//   //       <button onClick={() => props.updateCart(props.index)}>{props.item.name}</button>
+//   //     )
+//   //   }
+
+//   return (
+//     <div className="App">
+//       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+
+//       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+//         <p>Bakery Item {index}</p> // replace with BakeryItem component
+//       ))}
+
+//       <div>
+//         <h2>Cart</h2>
+//         {/* TODO: render a list of items in the cart */}
+//       </div>
+//     </div>
+//   );
 
 export default App;
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,19 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItems(props){
+    // specify return
+    const item = props.item
+    const addFunction = () =>
+        {props.setCart([...props.cart, item]) 
+        props.setCount(props.count+item.price)}
+    // const addCount = () =>
+    return (
+        <div>
+            {/* displaying the items */}
+            <div>{item.name}</div>
+            <button onClick={addFunction}>Add</button>
+            <div>{item.description}</div>
+            <div>{item.price}</div>
+            <img src={item.image}/>
+        </div>
+    )
+}
